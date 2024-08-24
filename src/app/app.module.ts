5a33f24e4ba7e5b310755d946938f47aed7ba522
Wilson Piña <wilsonale@hotmail.com>
2024-08-24 12:04:07 +0200

    calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+const routes: Routes = [
+  {path: '', component: CalculadoraComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
